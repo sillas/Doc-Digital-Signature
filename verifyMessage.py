3265ed40e98e02882485aa6d111148939500bd80
Sillas <deepsky.sill@gmail.com>
2020-10-02 20:01:14 -0300

    Check for all signature files in the folder

--- verifyMessage.py
+++ verifyMessage.py
@@ -1,6 +1,6 @@
 # pip install pycryptodome
 # https://cryptobook.nakov.com/digital-signatures/rsa-sign-verify-examples
-
+import os
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA384
@@ -17,13 +17,17 @@ ap.add_argument("-i", "--input", required=True,
 ap.add_argument("-k", "--keys", required=False,
    help="The filename of the keys file without extension (Optional. If not given, assume the name 'key_pen')")
 
-ap.add_argument("-s", "--signature", required=True,
+ap.add_argument("-s", "--signature", required=False,
    help="The signature name (stored in \"signatures\" folder)")
 
 args = vars(ap.parse_args())
 
 keys_file_name = args['keys'] or "key_pen"
-signature = args['signature']
+signatures = [args['signature']] if args['signature'] != None else []
+
+for filename in os.listdir('signatures'):
+    if( ".sign" in filename ):
+        signatures.append( filename.replace('.sign', '') )
 
 # ----------------------------------------------------------
 
@@ -32,27 +36,37 @@ try:
     with open( args['input'], 'rb') as m:
         message = m.read()
 
-    # Verify valid PKCS#1 v1.5 signature (RSAVP1)
-    with open(f'signatures/{ signature }.sign', 'rb') as sign_key:
-        signature = sign_key.read()
-
     with open(f'.keys/{ keys_file_name }.pub', 'rb') as pub_key:
         pubKey = RSA.import_key( pub_key.read() )
 
 except Exception as e:
-    print('ERRO:', e)
+    print('ERROR 1:', e)
     exit()
 
 hash = SHA384.new( message )
 verifier = PKCS1_v1_5.new( pubKey )
 
-if( verifier.verify( hash, signature ) ):
-    print("Everything is ok!\n")
+try:
+    
+    for signature in signatures:
+        # Verify valid PKCS#1 v1.5 signature (RSAVP1)
+        with open(f'signatures/{ signature }.sign', 'rb') as sign_key:
+            sign = sign_key.read()
 
-else:
+        if( verifier.verify( hash, sign ) ):
+            print("Everything is ok!")
+            print(f"Verified with { signature }.sign file\n")
+            exit()
+
+
     print()
     print("##############################################")
     print("#           |                                #")
-    print("#  Caution! | The message has been modified. #")
+    print("#           | The message has been modified, #")
+    print("#  Caution! | or there is no signature for   #")
+    print("#           | this file.                     #")
     print("#           |                                #")
     print("##############################################\n")
+
+except Exception as e:
+    print('ERROR 2:', e)
